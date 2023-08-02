e32c07fb91fe86e833f6d5d0466bf2e6555e9346
Larissa Stack <larissastack@gmail.com>
2023-08-01 22:41:57 -0700

    Finied working on the functions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 
 const mysql = require('mysql2');
 
-const db = require('./connection');
+const db = require('./db/connection');
 
 inquirer
     .prompt([
@@ -11,13 +11,13 @@ inquirer
             type: 'list',
             message: 'What would you like to do?',
             choices: [
-                'Update Employee Role',
+                'View Employee Role',
+                'Add Employee',
                 'View All Roles',
                 'Add Role',
                 'View All Departments',
                 'Add Department',
                 'Quit',
-                'Add Employee',
             ],
         },
     ])
@@ -36,6 +36,7 @@ inquirer
                 console.log('view all roles');
                 break;
             case 'Add Role':
+                addRole();
                 console.log('add role');
                 break;
             case 'View All Departments':
@@ -43,7 +44,8 @@ inquirer
                 console.log('view all departments');
                 break;
             case 'Add Department':
-                consolelog('ad department');
+                addDepartment();
+                console.log('add department');
                 break;
             case 'Quit':
                 console.log('quit');
@@ -60,41 +62,19 @@ function viewAllEmployees() {
         console.log(results);
     });
 }
-//Function to view all roles
-function viewAllRoles() {
-    db.query('SELECT * FROM role', (err, results) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log(results);
-    });
-}
-
-//Function to view all departments
-function viewAllDepartments() {
-    db.query('SELECT * FROM department', (err, results) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log(results);
-    });
-}
 
 //function to find managers
-function findManagers() {
-    db.promise()
-        .query('SELECT * FROM employee WHERE manager_id IS NULL')
-        .then(([rows, fields]) => {
-            console.log(rows);
-            const managerChoices = rows.map((row) => {
-                return {
-                    name: `${row.first_name} ${row.last_name}`,
-                    value: row.id,
-                };
-            });
-            // console.log(managerChoices);
-            return managerChoices;
-        });
+async function findManagers() {
+    const [rows, fields] = await db
+        .promise()
+        .query('SELECT * FROM employee WHERE manager_id IS NULL');
+    let managerChoices = rows.map((manager) => {
+        return {
+            name: `${manager.first_name} ${manager.last_name}`,
+            value: manager.id,
+        };
+    });
+    return managerChoices;
 }
 
 //Function to add employee
@@ -113,7 +93,8 @@ function addEmployee() {
             return roleChoices;
         })
         .then((roleChoices) => {
-            db.promise();
+            let managerChoices = findManagers();
+            console.log('manager choices', managerChoices);
             inquirer
                 .prompt([
                     {
@@ -132,24 +113,94 @@ function addEmployee() {
                         message: 'What is the employees role?',
                         choices: roleChoices,
                     },
-                    {
-                        name: 'manager_name',
-                        type: 'list',
-                        message: 'Who is the employees manager?',
-                        choices: managerChoices,
-                    },
+                    // {
+                    //     name: 'manager_name',
+                    //     type: 'list',
+                    //     message: 'Who is the employees manager?',
+                    //     choices: managerChoices,
+                    // },
                 ])
-                .then((answer) => {
-                    console.log(answer);
-                    // db.query('INSERT INTO employee SET ?', (err, results) => {
-                    //     if (err) {
-                    //         console.log(err);
-                    //     }
-                    //     console.log(results);
-                    // });
+                .then((employee) => {
+                    return this.connection
+                        .promise()
+                        .query('INSERT INTO employee SET ?', employee);
                 });
         });
 }
 
-//Function to add role
-// function addRole() {
+//Function to view all roles
+function viewAllRoles() {
+    db.query('SELECT * FROM role', (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(results);
+    });
+}
+
+//Function to add a role
+function addRole() {
+    db.promise()
+        .query('SELECT * FROM department')
+        .then(([rows, fields]) => {
+            console.log(rows);
+            const departmentChoices = rows.map((row) => {
+                return {
+                    name: row.name,
+                    value: row.id,
+                };
+            });
+            console.log(departmentChoices);
+            return departmentChoices;
+        })
+        .then((departmentChoices) => {
+            inquirer
+                .prompt([
+                    {
+                        name: 'title',
+                        type: 'input',
+                        message: 'What is the title of the role?',
+                    },
+                    {
+                        name: 'salary',
+                        type: 'input',
+                        message: 'What is the salary of the role?',
+                    },
+                    {
+                        name: 'department_id',
+                        type: 'list',
+                        message: 'What is the department of the role?',
+                        choices: departmentChoices,
+                    },
+                ])
+                .then((answer) => {
+                    console.log(answer);
+                    db.query('INSERT INTO role SET ?', (err, results) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        console.log(results);
+                    });
+                });
+        });
+}
+
+//Function to view all departments
+function viewAllDepartments() {
+    db.query('SELECT * FROM department', (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(results);
+    });
+}
+
+//Function to add a department
+function addDepartment() {
+    db.query('INSERT INTO department SET ?', (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(results);
+    });
+}
